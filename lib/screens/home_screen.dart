9c7371c3b1dba47e62e75e1d29b8c2f0343c4a9a
徐近庭 Hsu, Chin Ting <steven910529@gmail.com>
2024-05-02 20:04:38 +1000

    home screen rendering: add more buttons and texts

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,7 +17,7 @@ class HomeScreen extends StatelessWidget {
               const SizedBox(height: 10),
               Container(
                 width: 300.0,
-                height: 600.0,
+                height: 620.0,
                 decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -53,10 +53,14 @@ class HomeScreen extends StatelessWidget {
                           bottomRight: Radius.circular(20.0),
                         ),
                       ),
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 30),
-                          const Text(
+                      child: OutlinedButton(
+                          onPressed: state.redirectHome,
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                          child: const Text(
                             'Redeem Now',
                             textDirection: TextDirection.ltr,
                             style: TextStyle(
@@ -66,10 +70,100 @@ class HomeScreen extends StatelessWidget {
                               fontFamily: "Italic",
                             ),
                           ),
-                          const SizedBox(height: 30),
-                        ],
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 20),
+                      const SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                            "    Recent Sales",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 2, 6, 9),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Italic",
+                            ),
+                          ),
+                      ),
+                      const SizedBox(height: 20),
+                    Container(
+                      width: 240.0,
+                      height: 120.0,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFCFDEE0),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                      ),
+                      child: OutlinedButton(
+                          onPressed: state.redirectHome,
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                          child: const Text(
+                            'Recent sale',
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 12, 7, 9),
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Italic",
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                            "    Earn Points",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 2, 6, 9),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Italic",
+                            ),
+                          ),
+                      ),
+                      const SizedBox(height: 20),
+                    Container(
+                      width: 240.0,
+                      height: 120.0,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFCFDEE0),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                      ),
+                      child: OutlinedButton(
+                          onPressed: state.redirectHome,
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                          child: const Text(
+                            'Earn points',
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 7, 1, 2),
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Italic",
+                            ),
+                          ),
+                        ),
+                      ),
                   ],
                 ),
               ),
