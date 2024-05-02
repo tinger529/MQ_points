4cff7de21bd687d1386062c512a55e118f1fe55d
徐近庭 Hsu, Chin Ting <steven910529@gmail.com>
2024-05-02 21:17:24 +1000

    create patterns.dart

---
+++ lib/models/patterns.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+Container buildContainer(state, String txt, {Color colour = const Color(0xFF6FC5C5), Color fontColor = const Color(0xFFA83759)}) {
+    return Container(
+      width: 240.0,
+      height: 120.0,
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+          bottomLeft: Radius.circular(20.0),
+          bottomRight: Radius.circular(20.0),
+        ),
+      ),
+      child: OutlinedButton(
+          onPressed: state.redirectHome,
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+          child: Text(
+            txt,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: fontColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: "Italic",
+            ),
+          ),
+        ),
+      );
+  }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,44 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/state.dart';
+import '../models/patterns.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  // write a function that return a container
-  Container _buildContainer(state, String txt, {Color colour = const Color(0xFF6FC5C5), Color fontColor = const Color(0xFFA83759)}) {
-    return Container(
-      width: 240.0,
-      height: 120.0,
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0),
-          bottomLeft: Radius.circular(20.0),
-          bottomRight: Radius.circular(20.0),
-        ),
-      ),
-      child: OutlinedButton(
-          onPressed: state.redirectHome,
-          style: OutlinedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-          ),
-          child: Text(
-            txt,
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: fontColor,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              fontFamily: "Italic",
-            ),
-          ),
-        ),
-      );
-  }
   
   @override
   Widget build(context) {
@@ -76,7 +42,7 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(height: 20),
-                    _buildContainer(state, "Redeem Now!"),
+                    buildContainer(state, "Redeem Now!"),
                     const SizedBox(height: 20),
                     const SizedBox(
                       width: double.infinity,
@@ -92,7 +58,7 @@ class HomeScreen extends StatelessWidget {
                         ),
                     ),
                     const SizedBox(height: 20),
-                    _buildContainer(state, 'Recent sales', colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2)),
+                    buildContainer(state, 'Recent sales', colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2)),
                     const SizedBox(height: 20),
                     const SizedBox(
                       width: double.infinity,
@@ -108,7 +74,7 @@ class HomeScreen extends StatelessWidget {
                         ),
                     ),
                       const SizedBox(height: 20),
-                      _buildContainer(state, 'Earn points', colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2)),
+                      buildContainer(state, 'Earn points', colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2)),
                       const SizedBox(height: 10),
                       Image.asset(
                         'assets/image/toolbar.png',
