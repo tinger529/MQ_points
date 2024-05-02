3c792547a5ef38ac78d1d1e7cc5b6d03b34ca3d0
徐近庭 Hsu, Chin Ting <steven910529@gmail.com>
2024-05-02 20:59:59 +1000

    home screen rendering: seperate components

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,41 @@ import '../models/state.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  // write a function that return a container
+  Container _buildContainer(state, String txt, {Color colour = const Color(0xFF6FC5C5), Color fontColor = const Color(0xFFA83759)}) {
+    return Container(
+      width: 240.0,
+      height: 120.0,
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+          bottomLeft: Radius.circular(20.0),
+          bottomRight: Radius.circular(20.0),
+        ),
+      ),
+      child: OutlinedButton(
+          onPressed: state.redirectHome,
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+          child: Text(
+            txt,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: fontColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: "Italic",
+            ),
+          ),
+        ),
+      );
+  }
   
   @override
   Widget build(context) {
@@ -41,129 +76,39 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(height: 20),
-                    Container(
-                      width: 240.0,
-                      height: 120.0,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFF6FC5C5),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20.0),
-                          topRight: Radius.circular(20.0),
-                          bottomLeft: Radius.circular(20.0),
-                          bottomRight: Radius.circular(20.0),
-                        ),
-                      ),
-                      child: OutlinedButton(
-                          onPressed: state.redirectHome,
-                          style: OutlinedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                          ),
-                          child: const Text(
-                            'Redeem Now',
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                              color: Color(0xFFA83759),
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: "Italic",
-                            ),
+                    _buildContainer(state, "Redeem Now!"),
+                    const SizedBox(height: 20),
+                    const SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                          "    Recent Sales",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 2, 6, 9),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Italic",
                           ),
                         ),
-                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    _buildContainer(state, 'Recent sales', colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2)),
+                    const SizedBox(height: 20),
+                    const SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                          "    Earn Points",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 2, 6, 9),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Italic",
+                          ),
+                        ),
+                    ),
                       const SizedBox(height: 20),
-                      const SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                            "    Recent Sales",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 2, 6, 9),
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: "Italic",
-                            ),
-                          ),
-                      ),
-                      const SizedBox(height: 20),
-                    Container(
-                      width: 240.0,
-                      height: 120.0,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFCFDEE0),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20.0),
-                          topRight: Radius.circular(20.0),
-                          bottomLeft: Radius.circular(20.0),
-                          bottomRight: Radius.circular(20.0),
-                        ),
-                      ),
-                      child: OutlinedButton(
-                          onPressed: state.redirectHome,
-                          style: OutlinedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                          ),
-                          child: const Text(
-                            'Recent sale',
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 12, 7, 9),
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: "Italic",
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      const SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                            "    Earn Points",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 2, 6, 9),
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: "Italic",
-                            ),
-                          ),
-                      ),
-                      const SizedBox(height: 20),
-                    Container(
-                      width: 240.0,
-                      height: 120.0,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFCFDEE0),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20.0),
-                          topRight: Radius.circular(20.0),
-                          bottomLeft: Radius.circular(20.0),
-                          bottomRight: Radius.circular(20.0),
-                        ),
-                      ),
-                      child: OutlinedButton(
-                          onPressed: state.redirectHome,
-                          style: OutlinedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                          ),
-                          child: const Text(
-                            'Earn points',
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 7, 1, 2),
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: "Italic",
-                            ),
-                          ),
-                        ),
-                      ),
+                      _buildContainer(state, 'Earn points', colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2)),
                       const SizedBox(height: 10),
                       Image.asset(
                         'assets/image/toolbar.png',
