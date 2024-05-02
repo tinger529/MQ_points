67587c90b9527f18fd3605fc84ea2be92e21ae5b
徐近庭 Hsu, Chin Ting <steven910529@gmail.com>
2024-05-02 17:33:30 +1000

    home screen rendering: create redeem button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,80 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../models/state.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   
   @override
   Widget build(context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const SizedBox(height: 50),
-          const Text(
-            'MQ POINTS',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: Color.fromARGB(255, 2, 6, 9),
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              fontFamily: "Italic",
-            ),
-          ),
-          const SizedBox(height: 30),
-          Container(
-            width: 300.0,
-            height: 350.0,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
-                bottomLeft: Radius.circular(20.0),
-                bottomRight: Radius.circular(20.0),
+    return Consumer<StateModel>(
+      builder: (context, state, child) {
+        return Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 10),
+              Container(
+                width: 300.0,
+                height: 600.0,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 50),
+                    const Text(
+                      'MQ POINTS: 100',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 2, 6, 9),
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Italic",
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: 240.0,
+                      height: 120.0,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF6FC5C5),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 30),
+                          const Text(
+                            'Redeem Now',
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                              color: Color(0xFFA83759),
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Italic",
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            child: const Column(
-              children: [
-                SizedBox(height: 50),
-                SizedBox(
-                  width: 250,
-                  height: 50,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Student ID',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30),
-                SizedBox(
-                  width: 250,
-                  height: 50,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed: null,
-                  child: Text('Login'),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Image.asset(
-            'assets/image/coin.jpeg',
-            width: 200,
-            height: 150,
-            opacity: const AlwaysStoppedAnimation(.8),
-          ),
-        ]
+              const SizedBox(height: 20),
+            ]
       )
+    );
+      }
     );
   }
 }
