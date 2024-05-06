b35af174dbb7897e33c816944ddbc5d5d57680ce
徐近庭 Hsu, Chin Ting <steven910529@gmail.com>
2024-05-06 23:36:54 +1000

    setting page: add user photo and barcode

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -26,7 +26,7 @@ class SettingScreen extends StatelessWidget {
                   Container(
                     width: 300.0,
                     height: 650.0,
-                    padding: const EdgeInsets.only(left: 25, bottom: 24, right: 25, top: 24),
+                    padding: const EdgeInsets.only(left: 25, bottom: 24, right: 25, top: 10),
                     decoration: BoxDecoration(
                       color: const Color(0xFFF2FEFE),
                       borderRadius: BorderRadius.circular(20.0),
@@ -39,8 +39,12 @@ class SettingScreen extends StatelessWidget {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            tool(state, "home", 'assets/image/home.png'),
-                            const SizedBox(width: 10),
+                            Image.asset(
+                              'assets/image/user.png',
+                              width: 100,
+                              height: 100,
+                            ),
+                            const SizedBox(width: 30),
                             const Text(
                               "User name\nid:12345678\npoints: 100",
                               textAlign: TextAlign.left,
@@ -52,7 +56,12 @@ class SettingScreen extends StatelessWidget {
                           ],
                         ),
 
-                        const SizedBox(height: 10),
+                        Image.asset(
+                          'assets/image/barcode.png',
+                          width: 200,
+                          height: 150,
+                          opacity: const AlwaysStoppedAnimation(.8),
+                        ),
 
                         buildContainerF(state, "history", "History"),
 
