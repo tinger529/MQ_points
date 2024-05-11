9d887d81aac098cde7eed5be22796256c93bf79d
徐近庭 Hsu, Chin Ting <steven910529@gmail.com>
2024-05-11 10:59:35 +1000

    voucher page: improve rendering

--- lib/screens/voucher_screen.dart
+++ lib/screens/voucher_screen.dart
@@ -28,101 +28,109 @@ class VoucherScreen extends StatelessWidget {
 
         var vouchers = pair_vouchers(state.currentVouchers);
 
-        return Center(
+        return Stack(
+          children: [
+            Center(
 
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
 
-              const SizedBox(height: 10), // padding on top
+                  const SizedBox(height: 10), // padding on top
 
-              Container(  // white box in the center
-                width: 300.0,
-                height: 650.0,
-                decoration: const BoxDecoration(
-                  color: Color(0xFFF2FEFE),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0),
-                    bottomLeft: Radius.circular(20.0),
-                    bottomRight: Radius.circular(20.0),
-                  ),
-                ),
-
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-
-                    Text(   // MQ points counter
-                      'MQ POINTS: ${state.currentPoints}',
-                      textDirection: TextDirection.ltr,
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 2, 6, 9),
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Italic",
+                  Container(  // white box in the center
+                    width: 300.0,
+                    height: 650.0,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFF2FEFE),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        topRight: Radius.circular(20.0),
+                        bottomLeft: Radius.circular(20.0),
+                        bottomRight: Radius.circular(20.0),
                       ),
                     ),
-                    
-                    const SizedBox(height: 20),
 
-                    const SizedBox(   // Display text
-                      width: double.infinity,
-                      child: Text(
-                          "   My vouchers",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+
+                        Text(   // MQ points counter
+                          'MQ POINTS: ${state.currentPoints}',
+                          textDirection: TextDirection.ltr,
+                          style: const TextStyle(
                             color: Color.fromARGB(255, 2, 6, 9),
-                            fontSize: 16,
+                            fontSize: 24,
                             fontWeight: FontWeight.bold,
                             fontFamily: "Italic",
                           ),
                         ),
-                    ),
+                        
+                        const SizedBox(height: 20),
 
-                    const SizedBox(height: 20),
-
-                    ...vouchers.map((v) {
-                      if(v.length == 2){
-                        return buildContainerD(state, "item", v[0].getName, colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2));
-                      } 
-                      else{
-                        return Align(
-                  alignment: Alignment.centerLeft,
-                  child: buildContainerC(state, "item", v[0].getName, colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2))               
-                );
-                      }
-                    }),
-
-                    const SizedBox(height: 20),
-
-                    const SizedBox(   // Display text
-                      width: double.infinity,
-                      child: Text(
-                          "  Expired",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 2, 6, 9),
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Italic",
-                          ),
+                        const SizedBox(   // Display text
+                          width: double.infinity,
+                          child: Text(
+                              "   My vouchers",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 2, 6, 9),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: "Italic",
+                              ),
+                            ),
                         ),
+
+                        const SizedBox(height: 20),
+
+                        ...vouchers.map((v) {
+                          if(v.length == 2){
+                            return buildContainerD(state, "item", v[0].getName, colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2));
+                          } 
+                          else{
+                            return Align(
+                      alignment: Alignment.centerLeft,
+                      child: buildContainerC(state, "item", v[0].getName, colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2))               
+                    );
+                          }
+                        }),
+
+                        const SizedBox(height: 20),
+
+                        const SizedBox(   // Display text
+                          width: double.infinity,
+                          child: Text(
+                              "  Expired",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 2, 6, 9),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: "Italic",
+                              ),
+                            ),
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        buildContainerD(state, "item", 'Popular', colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2)),
+                          
+                      ],
                     ),
+                  ),
+                ]
+              )
+            ),
 
-                    const SizedBox(height: 20),
-
-                    buildContainerD(state, "item", 'Popular', colour: const Color(0xFFCFDEE0), fontColor: const Color.fromARGB(255, 7, 1, 2)),
-                      
-                    const SizedBox(height: 10),
-                      
-                    toolbar(state),
-                  ],
-                ),
+            Positioned.fill(
+              top: 600,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: toolbar(state),               
               ),
-            ]
-          )
-        );
+            ),
+          ]);
       }
     );
   }
